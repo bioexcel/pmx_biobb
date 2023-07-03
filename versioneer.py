e304f96fe2b5a1c55aa978ad4372aab899770411
Pau Andrio <andriopau@gmail.com>
2023-07-03 14:04:19 +0200

    Removing broken classifiers from setup

--- versioneer.py
+++ versioneer.py
@@ -1405,7 +1405,7 @@ def get_versions(verbose=False):
     Returns dict with two keys: 'version' and 'full'.
     """
 
-    return {"version": "4.1.0", "full-revisionid": "4.1.0",
+    return {"version": "4.1.1", "full-revisionid": "4.1.1",
             "dirty": None, "error": None,
             "date": None}
 
